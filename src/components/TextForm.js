22fa4095b8dfafe209357af39a395f78335cb707
Anish Tharu <namoanishtharu@gmail.com>
2022-05-16 15:10:02 +0530

    Some fixings & TextUtil is done-v23

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -128,7 +128,7 @@ export default function TextForm(props) {
         <h3>Your Text Summary</h3>
         <p>
           {
-            text.split(" ").filter((element) => {
+            text.split(/\s+/).filter((element) => {
               return element.length !== 0;
             }).length
           }{" "}
